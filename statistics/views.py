a7ff0f007f3393f23a69251644330d5ce2eb3a57
Anjali0685 <anjali.gupta1@gmail.com>
2017-05-17 12:46:29 +0530

    http status changed

--- statistics/views.py
+++ statistics/views.py
@@ -11,18 +11,18 @@ def get_scores(request, topic=''):
     Reads file/db for reviews and ratings for a hotel.
     '''
     if topic:
-        # If data required only as per file, pass hotel_id as well.
         params = { 'topic': topic }
     else:
         params = {}
+
     if request.method == 'POST':
         file_data = request.FILES.get('file')
         if not file_data:
-            return JsonResponse({'Status': 400, 'Message': 'Missing File.'})
+            return JsonResponse({'Status': 400, 'Message': 'Missing File.'}, status=400)
         try:
             data = json.load(file_data)
         except Exception as e:
-            return JsonResponse({'Status': 400, 'Message': 'Invalid file'})
+            return JsonResponse({'Status': 400, 'Message': 'Invalid file'}, status=400)
         
         hotel = tasks.get_hotel_info(data['HotelInfo'])
         tasks.update_hotel_reviews(data, hotel)
@@ -32,6 +32,6 @@ def get_scores(request, topic=''):
         resp = analytics.get_analytics(**params)
 
     if not resp:
-            return JsonResponse({'Status': 404, 'Message': 'Invalid topic requested'})
+            return JsonResponse({'Status': 404, 'Message': 'Invalid topic requested'}, status=404)
 
     return JsonResponse(resp)
